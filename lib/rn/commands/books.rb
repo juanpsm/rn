1f5526d6b3d77c1aa1f57fe040c33a9258785780
Juan Pablo Sánchez Magariños <juanpsanchezm@gmail.com>
2020-10-30 04:15:19 -0300

    Completo CRUD de Notes y Books

--- lib/rn/commands/books.rb
+++ lib/rn/commands/books.rb
@@ -1,6 +1,40 @@
 module RN
   module Commands
     module Books
+      GLOBAL_BOOK = "#{__dir__}/../../../files"
+
+      def self.path(name)
+        File.absolute_path("#{GLOBAL_BOOK}/#{name}")
+      end
+
+      def self.rootContent
+        Dir["#{GLOBAL_BOOK}/*"]
+      end 
+
+      def self.rootNotesRoutes
+        self.rootContent.reject{|file| File.directory?(file)}
+      end
+
+      def self.rootNotes
+        self.rootNotesRoutes.each.collect{|route| File.basename(route, ".*")}
+      end
+
+      def self.getNotes(name)
+        Dir["#{self.path(name)}/*"].reject{|file| File.directory?(file)}.each.collect{|route| File.basename(route, ".*")}
+      end
+
+      def self.getAllRoutes
+        self.rootContent.select{|file| File.directory?(file)}
+      end
+
+      def self.getAllNames
+        self.getAllRoutes.each.collect{|route| File.basename(route)}
+      end
+
+      def self.exists?(name)
+        File.directory?(Books.path(name))
+      end
+
       class Create < Dry::CLI::Command
         desc 'Create a book'
 
@@ -12,7 +46,16 @@ module RN
         ]
 
         def call(name:, **)
-          warn "TODO: Implementar creación del cuaderno de notas con nombre '#{name}'.\nPodés comenzar a hacerlo en #{__FILE__}:#{__LINE__}."
+          # warn "TODO: Implementar creación del cuaderno de notas con nombre '#{name}'.\nPodés comenzar a hacerlo en #{__FILE__}:#{__LINE__}."
+          dir = Books.path(name)
+          if Books.exists?(name)
+            warn "El cuaderno ya existe"
+            return false
+          end
+          FileUtils.mkdir_p(dir)
+          warn "Directorio: #{dir}"
+          warn "Created book: #{name}"
+          return true
         end
       end
 
@@ -30,7 +73,28 @@ module RN
 
         def call(name: nil, **options)
           global = options[:global]
-          warn "TODO: Implementar borrado del cuaderno de notas con nombre '#{name}' (global=#{global}).\nPodés comenzar a hacerlo en #{__FILE__}:#{__LINE__}."
+          # warn "TODO: Implementar borrado del cuaderno de notas con nombre '#{name}' (global=#{global}).\nPodés comenzar a hacerlo en #{__FILE__}:#{__LINE__}."
+          files = Book.rootContent
+          if global then
+            files.each do
+              |file| 
+              unless File.directory?(file) then
+                FileUtils.remove(file)
+                puts "Removed #{file}"
+              end
+            end
+            return true
+          else
+            dir = Books.path(name)
+            if Books.exists?(name)
+              FileUtils.rm_rf(dir)
+              warn "Removed #{dir}"
+              return true
+            else
+              warn "No existe el directorio #{dir}"
+              return false
+            end
+          end
         end
       end
 
@@ -42,7 +106,10 @@ module RN
         ]
 
         def call(*)
-          warn "TODO: Implementar listado de los cuadernos de notas.\nPodés comenzar a hacerlo en #{__FILE__}:#{__LINE__}."
+          # warn "TODO: Implementar listado de los cuadernos de notas.\nPodés comenzar a hacerlo en #{__FILE__}:#{__LINE__}."
+          books = Books.getAllNames
+          # puts "\nBooks: #{books}"
+          books.each_slice(5) { |row| puts row.map{|e| "%10s" % e}.join("  ") }
         end
       end
 
@@ -59,7 +126,17 @@ module RN
         ]
 
         def call(old_name:, new_name:, **)
-          warn "TODO: Implementar renombrado del cuaderno de notas con nombre '#{old_name}' para que pase a llamarse '#{new_name}'.\nPodés comenzar a hacerlo en #{__FILE__}:#{__LINE__}."
+          # warn "TODO: Implementar renombrado del cuaderno de notas con nombre '#{old_name}' para que pase a llamarse '#{new_name}'.\nPodés comenzar a hacerlo en #{__FILE__}:#{__LINE__}."
+          old_dir = Books.path(old_name)
+          new_dir = Books.path(new_name)
+          if (!Books.exists?(old_name) || Books.exists?(new_name))
+            warn "Book '#{old_name}' does not exist." unless Books.exists?(old_name)
+            warn "Book '#{new_name}' already exists." if Books.exists?(new_name)
+            return false
+          end
+          FileUtils.mv old_dir, new_dir
+          warn "Renamed book '#{old_name}' to '#{new_name}'"
+          return true
         end
       end
     end
--- lib/rn/commands/notes.rb
+++ lib/rn/commands/notes.rb
@@ -1,6 +1,42 @@
 module RN
   module Commands
     module Notes
+
+      def self.path(name, book=nil)
+        Books.path(book)
+        File.join("#{Books.path(book)}", "#{name}.txt")
+      end
+
+      def self.exists?(name, book=nil)
+        File.file?(self.path(name, book))
+      end
+
+      def self.editor(file)
+        prompt = "RN>> "
+        eof = "EON"
+        eof_feedback = " [End Of Note]\n"
+        File.open(file, File::RDWR|File::CREAT, 0644) {|f|
+          f.flock(File::LOCK_EX)
+          f.rewind
+          print "\nWrite the contents of the note below.\nYou can write multiple lines.\nEnd the note with '#{eof}' + [Enter].\n\n#{prompt}"
+          content = ""
+          input_line = STDIN.gets
+            while input_line.chomp != eof
+              content << input_line
+              print "#{prompt}"
+              input_line = STDIN.gets
+            end
+          print eof_feedback
+          f.write content
+          f.truncate(f.pos)
+        }
+      end
+
+      def self.viewer(file)
+        puts "Title: #{File.basename(file, ".*")}"
+        File.foreach(file) { |line| puts line }
+      end
+
       class Create < Dry::CLI::Command
         desc 'Create a note'
 
@@ -15,7 +51,20 @@ module RN
 
         def call(title:, **options)
           book = options[:book]
-          warn "TODO: Implementar creación de la nota con título '#{title}' (en el libro '#{book}').\nPodés comenzar a hacerlo en #{__FILE__}:#{__LINE__}."
+          # warn "TODO: Implementar creación de la nota con título '#{title}' (en el libro '#{book}').\nPodés comenzar a hacerlo en #{__FILE__}:#{__LINE__}."
+          if book
+            Books::Create.new.call(name:book) unless Books.exists?(book)
+          end
+          file = Notes.path(title, book)
+          if Notes.exists?(title, book)
+            warn "A note titled '#{title}' already exists#{" in book '#{book}'" if book}."
+            return false
+          end
+          File.new(file, "w+")
+          warn "Created note '#{title}'#{" in book '#{book}'" if book}."
+          puts "file: #{file}"
+          Notes.editor(file)
+          return true
         end
       end
 
@@ -33,12 +82,21 @@ module RN
 
         def call(title:, **options)
           book = options[:book]
-          warn "TODO: Implementar borrado de la nota con título '#{title}' (del libro '#{book}').\nPodés comenzar a hacerlo en #{__FILE__}:#{__LINE__}."
+          # warn "TODO: Implementar borrado de la nota con título '#{title}' (del libro '#{book}').\nPodés comenzar a hacerlo en #{__FILE__}:#{__LINE__}."
+          file = Notes.path(title, book)
+          unless Notes.exists?(title, book)
+            warn "There is not any note titled '#{title}'#{" in book '#{book}'" if book}."
+            return false
+          end
+          File.delete(file)
+          warn "Deleted note '#{title}'#{" from book '#{book}'" if book}."
+          puts "file: #{file}"
+          return true
         end
       end
 
       class Edit < Dry::CLI::Command
-        desc 'Edit the content a note'
+        desc 'Edit the content of a note'
 
         argument :title, required: true, desc: 'Title of the note'
         option :book, type: :string, desc: 'Book'
@@ -51,7 +109,16 @@ module RN
 
         def call(title:, **options)
           book = options[:book]
-          warn "TODO: Implementar modificación de la nota con título '#{title}' (del libro '#{book}').\nPodés comenzar a hacerlo en #{__FILE__}:#{__LINE__}."
+          # warn "TODO: Implementar modificación de la nota con título '#{title}' (del libro '#{book}').\nPodés comenzar a hacerlo en #{__FILE__}:#{__LINE__}."
+          file = Notes.path(title, book)
+          unless Notes.exists?(title, book)
+            warn "There is not any note titled '#{title}'#{" in book '#{book}'" if book}."
+            return false
+          end
+          warn "Opened note '#{title}'#{" in book '#{book}'" if book}."
+          puts "file: #{file}"
+          Notes.editor(file)
+          return true
         end
       end
 
@@ -70,7 +137,17 @@ module RN
 
         def call(old_title:, new_title:, **options)
           book = options[:book]
-          warn "TODO: Implementar cambio del título de la nota con título '#{old_title}' hacia '#{new_title}' (del libro '#{book}').\nPodés comenzar a hacerlo en #{__FILE__}:#{__LINE__}."
+          # warn "TODO: Implementar cambio del título de la nota con título '#{old_title}' hacia '#{new_title}' (del libro '#{book}').\nPodés comenzar a hacerlo en #{__FILE__}:#{__LINE__}."
+          old_file = Notes.path(old_title, book)
+          new_file = Notes.path(new_title, book)
+          if (!Notes.exists?(old_title, book) || Notes.exists?(new_title, book))
+            warn "Note '#{old_title}' does not exist#{" in book '#{book}'" if book}." unless Notes.exists?(old_title, book)
+            warn "Note '#{new_title}' already exists#{" in book '#{book}'" if book}." if Notes.exists?(new_title, book)
+            return false
+          end
+          FileUtils.mv old_file, new_file
+          warn "Renamed note '#{old_title}' to '#{new_title}"
+          return true
         end
       end
 
@@ -90,7 +167,22 @@ module RN
         def call(**options)
           book = options[:book]
           global = options[:global]
-          warn "TODO: Implementar listado de las notas del libro '#{book}' (global=#{global}).\nPodés comenzar a hacerlo en #{__FILE__}:#{__LINE__}."
+          # warn "TODO: Implementar listado de las notas del libro '#{book}' (global=#{global}).\nPodés comenzar a hacerlo en #{__FILE__}:#{__LINE__}."
+          notes = []
+          notes << Books.rootNotes unless book
+          if !book && !global
+            Books.getAllNames.each do
+              |each|
+              Books.getNotes(each)
+              notes << Books.getNotes(each)
+            end
+          elsif book
+            Books.getNotes(book)
+            notes = Books.getNotes(book)
+          end
+          puts "#{"\nTodas las " unless global || book}Notas#{" del cuaderno '#{book}'" if book}:\n"
+          notes.flatten!
+          notes.each_slice(5) { |row| puts row.map{|e| "%10s" % e}.join("  ") }
         end
       end
 
@@ -108,7 +200,16 @@ module RN
 
         def call(title:, **options)
           book = options[:book]
-          warn "TODO: Implementar vista de la nota con título '#{title}' (del libro '#{book}').\nPodés comenzar a hacerlo en #{__FILE__}:#{__LINE__}."
+          # warn "TODO: Implementar vista de la nota con título '#{title}' (del libro '#{book}').\nPodés comenzar a hacerlo en #{__FILE__}:#{__LINE__}."
+          file = Notes.path(title, book)
+          unless Notes.exists?(title, book)
+            warn "There is not any note titled '#{title}'#{" in book '#{book}'" if book}."
+            return false
+          end
+          warn "Opened note '#{title}'#{" in book '#{book}'" if book}."
+          puts "file: #{file}"
+          Notes.viewer(file)
+          return true
         end
       end
     end
