747a9b00ff8dcccfc935a3146669b6d0739728ca
Juan Pablo Sánchez Magariños <juanpsanchezm@gmail.com>
2020-10-30 17:31:15 -0300

    mas pruebas

--- testeditor.rb
+++ testeditor.rb
@@ -1,16 +1,19 @@
+file = 'test.rn'
 prompt = "RN>> "
 eof = "EON"
-File.open("asd.txt", File::RDWR|File::CREAT, 0644) {|f|
+eof_feedback = " [End Of Note]\n"
+File.open(file, File::RDWR|File::CREAT, 0644) {|f|
   f.flock(File::LOCK_EX)
   f.rewind
   print "\nWrite the contents of the note below.\nYou can write multiple lines.\nEnd the note with '#{eof}' + [Enter].\n\n#{prompt}"
   content = ""
-  input_line = gets
+  input_line = STDIN.gets
     while input_line.chomp != eof
+      content << input_line
       print "#{prompt}"
-      input_line = gets
-      content << input_line unless input_line.chomp == eof
+      input_line = STDIN.gets
     end
+  print eof_feedback
   f.write content
   f.truncate(f.pos)
 }
