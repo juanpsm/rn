23e67d648d8c3c416dbccbb606ce0f4a52f10dcb
Juan Pablo Sánchez Magariños <juanpsanchezm@gmail.com>
2021-01-26 13:29:34 -0300

    agregar Rich Text a notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -64,6 +64,6 @@ class NotesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def note_params
-      params.require(:note).permit(:title, :book_id)
+      params.require(:note).permit(:title, :book_id, :content)
     end
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,4 @@
 class Note < ApplicationRecord
   belongs_to :book
+  has_rich_text :content
 end
