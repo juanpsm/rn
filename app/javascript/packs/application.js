b4c1da8d976d99231198defb272fa07e7b08656f
Juan Pablo Sánchez Magariños <juanpsanchezm@gmail.com>
2021-02-07 05:22:52 -0300

    dependencias etc

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import 'bootstrap'
+import 'bootstrap';
 // import 'css/styles' // delete? 
 import '../stylesheets/application';
 import "@fortawesome/fontawesome-free/js/all";
