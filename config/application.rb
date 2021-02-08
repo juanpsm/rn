cd1ecbd6f785bf5c75f7cbe26a9c512fca778681
Juan Pablo Sánchez Magariños <juanpsanchezm@gmail.com>
2021-02-08 06:47:44 -0300

    mas dependencias

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module Rn
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.eager_load_paths << Rails.root.join("extras")
     config.after_initialize do
       ActionText::ContentHelper.allowed_attributes.add 'style'
       ActionText::ContentHelper.allowed_attributes.add 'controls'
